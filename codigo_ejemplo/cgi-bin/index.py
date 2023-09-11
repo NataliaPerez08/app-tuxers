901502081cc2ecfcb5bc752593f2e6b1d5e91b74
DONMARCORS <marcoantonioriverasilva@ciencias.unam.mx>
2023-09-11 00:51:35 +0000

    Practica terminada

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -2,13 +2,30 @@
 import cgi
 
 print ("Content-type: text/html")
-print
-
 print ("""
-Hola Mundo
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <div id="login-container">
+""")
+form=cgi.FieldStorage()
+print ("<h2>user:", form["user"].value)
+print ("<h2>pass:", form["pass"].value)
+
+print("""
+    </div>
+    <img src="/images/eevee.gif" alt="Logo" />
+</body>
+</html>
 """)
 
-form=cgi.FieldStorage()
-print ("<p>user:", form["user"].value)
-print ("<p>pass:", form["pass"].value)
+#form=cgi.FieldStorage()
+#print ("<p>user:", form["user"].value)
+#print ("<p>pass:", form["pass"].value)
 
---
+++ codigo_ejemplo/js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Aquí puedes agregar la lógica de validación de usuario y contraseña.
+    // Por ejemplo, puedes comparar con valores fijos o realizar una solicitud AJAX.
+
+});
+
