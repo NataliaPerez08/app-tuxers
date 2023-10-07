1892f172fcc9d265f3e4864ee9da28710e6fca2e
root <root@app-server.northamerica-northeast2-a.c.practica-1-mars.internal>
2023-10-07 01:05:06 +0000

    inicio

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import cgi
+import psycopg2
 
 print ("Content-type: text/html")
 print ("""
@@ -13,10 +14,35 @@ print ("""
 </head>
 <body>
     <div id="login-container">
-""")
+    """)
 form=cgi.FieldStorage()
 print ("<h2>user:", form["user"].value)
 print ("<h2>pass:", form["pass"].value)
+
+try:
+    connection = psycopg2.connect(user = "admin",
+                                  password = "Tuxers2612",
+                                  host = "10.0.0.6",
+                                  port = "5432",
+                                  database = "proyecto1")
+
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    print ( connection.get_dsn_parameters(),"\n")
+
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record,"\n")
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+finally:
+    #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
 print("""
     </div>
